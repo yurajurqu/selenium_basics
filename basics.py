044422fc7b5cc221ae12e18cacd32057ba993316
obarboza <obarboza@pe.ibm.com>
2021-03-10 12:05:28 -0500

    clicking boton

--- basics.py
+++ basics.py
@@ -9,3 +9,6 @@ driver.get("https://duckduckgo.com")
 # search_btn = driver.find_element_by_id("search_form_input_homepage")
 search_input = driver.find_element_by_xpath("(//input[contains(@class,'js-search-input')])[1]")
 search_input.send_keys("My user agent")
+
+search_btn = driver.find_element_by_id("search_button_homepage")
+search_btn.click()
