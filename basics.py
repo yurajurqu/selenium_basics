b184d410313d07d759a7b6b9763bfa79fbda6030
obarboza <obarboza@pe.ibm.com>
2021-03-10 12:03:08 -0500

    sending input to input

--- basics.py
+++ basics.py
@@ -5,3 +5,7 @@ chrome_path= which("chromedriver")
 
 driver = webdriver.Chrome(executable_path=chrome_path)
 driver.get("https://duckduckgo.com")
+
+# search_btn = driver.find_element_by_id("search_form_input_homepage")
+search_input = driver.find_element_by_xpath("(//input[contains(@class,'js-search-input')])[1]")
+search_input.send_keys("My user agent")
