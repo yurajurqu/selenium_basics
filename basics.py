22c25a8eb4189113e89a31ba3510bec92d76a9f1
obarboza <obarboza@pe.ibm.com>
2021-03-10 12:06:45 -0500

    sending KEY (instead of clicking)

--- basics.py
+++ basics.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from shutil import which
 
 chrome_path= which("chromedriver")
@@ -9,6 +10,7 @@ driver.get("https://duckduckgo.com")
 # search_btn = driver.find_element_by_id("search_form_input_homepage")
 search_input = driver.find_element_by_xpath("(//input[contains(@class,'js-search-input')])[1]")
 search_input.send_keys("My user agent")
+search_input.send_keys(Keys.ENTER)
 
-search_btn = driver.find_element_by_id("search_button_homepage")
-search_btn.click()
+# search_btn = driver.find_element_by_id("search_button_homepage")
+# search_btn.click()
